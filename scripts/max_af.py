a07793305b719c04927b853f98e7f2e8c1f7e3b0
tdayris <thibault.dayris@gustaveroussy.fr>
2020-05-11 17:35:41 +0200

    [fix] (ETRO): KeyError III

--- scripts/max_af.py
+++ scripts/max_af.py
@@ -19,21 +19,21 @@ tsv = pandas.read_csv(
 
 # Add MAX AF information
 tsv_iterator = zip(
-    tsv["dbNSFP_ExAC_NFE_AF "],
-    tsv["dbNSFP_ExAC_SAS_AF "],
-    tsv["dbNSFP_ExAC_Adj_AF "],
-    tsv["dbNSFP_ExAC_AFR_AF "],
-    tsv["dbNSFP_ExAC_AF "],
-    tsv["dbNSFP_ExAC_FIN_AF "],
-    tsv["dbNSFP_1000Gp3_EUR_AF "],
-    tsv["dbNSFP_ExAC_AMR_AF "],
-    tsv["dbNSFP_ExAC_EAS_AF "],
-    tsv["dbNSFP_ESP6500_EA_AF "],
-    tsv["dbNSFP_1000Gp3_AMR_AF "],
-    tsv["dbNSFP_1000Gp3_AF "],
-    tsv["dbNSFP_1000Gp3_EAS_AF "],
-    tsv["dbNSFP_1000Gp3_EUR_AF "],
-    tsv["dbNSFP_1000Gp3_AFR_AF "],
+    tsv["dbNSFP_ExAC_NFE_AF"],
+    tsv["dbNSFP_ExAC_SAS_AF"],
+    tsv["dbNSFP_ExAC_Adj_AF"],
+    tsv["dbNSFP_ExAC_AFR_AF"],
+    tsv["dbNSFP_ExAC_AF"],
+    tsv["dbNSFP_ExAC_FIN_AF"],
+    tsv["dbNSFP_1000Gp3_EUR_AF"],
+    tsv["dbNSFP_ExAC_AMR_AF"],
+    tsv["dbNSFP_ExAC_EAS_AF"],
+    tsv["dbNSFP_ESP6500_EA_AF"],
+    tsv["dbNSFP_1000Gp3_AMR_AF"],
+    tsv["dbNSFP_1000Gp3_AF"],
+    tsv["dbNSFP_1000Gp3_EAS_AF"],
+    tsv["dbNSFP_1000Gp3_EUR_AF"],
+    tsv["dbNSFP_1000Gp3_AFR_AF"],
     tsv["dbNSFP_1000Gp3_SAS_AF"]
 )
 
